c933fe8d46811b80ea03707caac3cb64d94a1fa3
miguel <pursuit@MB-pro.local>
2019-09-26 10:43:00 -0400

    added variables playground inside folder unit 1, also added README file

---
+++ unit1/logic and variables.playground/Contents.swift
@@ -0,0 +1,82 @@
+import UIKit
+
+var str = "Hello, playground"
+
+/*
+ var = to declare a variable, you must declare it "make it" before you can use it and assign values to it
+ example: var name = "miguel"
+
+ 
+// = start a line comment
+
+ 
+ to create multiple lines of comments start with /* and end with */
+
+
+ type inference: implicit declaration of a variable, assigning a data type by the value stored
+ example: var fellowName = "miguel" //this is a string because "miguel" is a string value
+ 
+ 
+ type annotation: explicitly writing the data type using : followed by the data type name
+ example: var fellowName: String = "miguel"
+ 
+ 
+you can do math with variables by using operators
+ example: var Age = 27 + 3
+ or
+ var Age = 27
+    var futureAge = Age + 3
+ 
+string interpolation is embedding a variable into a string using \(variable name) syntax as seen below
+ 
+ print("Age of fellow 3 years from now is \(futureAge)")
+ 
+
+var Age = 27
+var futureAge = Age + 3
+
+
+print("the age of the fellow (\(Age)) 3 years from now is \(futureAge)")
+
+ re-assigning a value examples below
+ 
+ var Age = 11
+ 
+ Age = 11 + 11
+ or
+ Age = Age + 11
+ or
+ Age += 11 // this is called shorthand arithmetic
+ 
+ 
+ operators
+ 
+ && = and
+ 
+ || = or
+ 
+true  && true = true   -- example -- 1>0(true) && 2>1(true) = TRUE
+false && false = false -- example -- 1>2(false) && 2>4(false) = FALSE
+true  && false = false -- example -- 1>0(true) && 2>4(false) = FALSE
+ 
+
+false || false = false -- example -- 1>2(false) || 2>4(false) = FALSE
+true  || true = true   -- example -- 1>0(true) || 2>1(true) = TRUE
+true  || false = true  -- example -- 1>0(true) || 2>4(false) = TRUE
+ 
+
+ 
+comparison
+ n1 > n2 -- greater than
+ n1 < n2 -- less than
+ n1 == n2 -- equal to
+ n1 != n2 -- not equal to
+ 
+ */
+
+var even = 24.0/5.0
+
+print(even)
+
+
+
